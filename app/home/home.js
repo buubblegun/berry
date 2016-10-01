841c3297a4bf62a2a134e92e48d2b1553b3346f4
harryyyy <Harry@Yexings-MacBook-Pro.local>
2016-10-01 16:48:30 -0700

    passing lat/lng query to back

--- app/home/home.js
+++ app/home/home.js
@@ -1,16 +1,33 @@
 'use strict';
 
 angular.module('myApp.home', [
-    'ngRoute',
+        'ngRoute',
         'ngAutocomplete'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/home', {
             templateUrl: 'home/home.html',
             controller: 'HomeCtrl'
         });
     }])
 
-    .controller('HomeCtrl', ['$scope', function($scope) {
-        $scope.result = '';
+    .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.location = '';
+        $scope.locationDetails = '';
+
+
+        $scope.searchEvents = function () {
+            var lat = $scope.locationDetails.geometry.location.lat();
+            var lng = $scope.locationDetails.geometry.location.lng();
+            $http({
+                method: 'GET',
+                url: 'events/lat='+lat+'&'+'lng='+lng
+            }).then(function successCallback(response) {
+                console.log(response);
+                console.log("success");
+            }, function errorCallback(response) {
+                console.log(response);
+                console.log("fails");
+            });
+        }
     }]);
