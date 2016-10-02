cdf4626485e64d47a317a1730db9c112dffe8d28
Arjun <arjunsesh@github.com>
2016-10-01 18:00:26 -0700

    added integration code

--- backend/searchevents/views.py
+++ backend/searchevents/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from eb_fetch import eb_fetch
 def index(request): 
-    location = request.GET.get('lat','@37.7576171,-122.5776829,11z') #default SF
+    lat = request.GET.get('lat','@37.7576171') #default SF
+    lon = request.GET.get('long','-122.5776829')
     radius = request.GET.get('radius','5') #defaulted to 5 miles
-    return searchevents(location, radius)
+    return eb_fetch(lat, lon, radius)
 
 def searchevents(location, radius)
     print "hello"
