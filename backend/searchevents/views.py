5d0e27d39b618a7161b53e5bb735dab570faf14b
Arjun <arjunsesh@github.com>
2016-10-01 22:42:27 -0700

    added httprequest wrapped

--- backend/searchevents/views.py
+++ backend/searchevents/views.py
@@ -5,7 +5,7 @@ def index(request):
     lat = request.GET.get('lat','@37.7576171') #default SF
     lon = request.GET.get('long','-122.5776829')
     radius = request.GET.get('radius','5') #defaulted to 5 miles
-    return eb_fetch(lat, lon, radius)
+    return HttpResponse(eb_fetch(lat, lon, radius))
 
 def searchevents(location, radius):
     print "hello"
