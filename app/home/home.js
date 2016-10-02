878cb26374ca889ab93731162e5fbe04b9aded34
harryyyy <Harry@Yexings-MacBook-Pro.local>
2016-10-01 17:43:44 -0700

    table

--- app/home/home.js
+++ app/home/home.js
@@ -2,7 +2,8 @@
 
 angular.module('myApp.home', [
         'ngRoute',
-        'ngAutocomplete'])
+        'ngAutocomplete',
+        'ngAnimate'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/home', {
@@ -14,20 +15,44 @@ angular.module('myApp.home', [
     .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
         $scope.location = '';
         $scope.locationDetails = '';
+        $scope.events = '';
+        $scope.showSearch = 1;
+        $scope.showTable = 0;
 
 
         $scope.searchEvents = function () {
+            $scope.showSearch = 0;
             var lat = $scope.locationDetails.geometry.location.lat();
             var lng = $scope.locationDetails.geometry.location.lng();
             $http({
                 method: 'GET',
-                url: 'events/lat='+lat+'&'+'lng='+lng
+                url: 'events/lat='+lat+'&'+'lng='+lng+'radius=50'
             }).then(function successCallback(response) {
                 console.log(response);
                 console.log("success");
+                $scope.showTable = 1;
             }, function errorCallback(response) {
                 console.log(response);
                 console.log("fails");
+                $scope.events = [
+                    {
+                        "eventName": "eventName1",
+                        "eventLocation": "eventLocation1",
+                        "eventType": "eventType1",
+                        "eventDescription": "eventDescription1",
+                        "eventLink": "eventLink1",
+                        "eventDate": "eventDate1"
+                    },
+                    {
+                        "eventName": "eventName2",
+                        "eventLocation": "eventLocation2",
+                        "eventType": "eventType2",
+                        "eventDescription": "eventDescription2",
+                        "eventLink": "eventLink2",
+                        "eventDate": "eventDate2"
+                    }
+                ];
+                $scope.showTable = 1;
             });
         }
     }]);
